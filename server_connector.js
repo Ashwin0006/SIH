00859c6fc8897930aafc8df837129c8df9cef7e3
Ashwin0006 <ashcrafts0006@gmail.com>
2023-09-20 20:42:10 +0530

    Login and SignupIntegrated

--- server_connector.js
+++ server_connector.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const oracledb = require('oracledb');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors()); 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/static', express.static(__dirname + '/webpage'));
 
-app.listen(3000, () => {
-    console.log('Server started on http://localhost:3000');
+
+
+app.listen(3000, '0.0.0.0', () => {
+    console.log('Server started on http://0.0.0.0:3000');
 });
+
+app.use((req, res, next) => {
+    console.log(`${req.method} request for '${req.url}'`);
+    next();
+});
+
+/*app.listen(3000, () => {
+    console.log('Server started on http://localhost:3000');
+});*/
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/webpage/Signup.html');
@@ -42,6 +55,38 @@ app.post('/submit-signup', async (req, res) => {
     } finally {
         if (con) {
             con.close();
+        }
+    }
+});
+
+app.post('/login', async (req, res) => {
+    let connection;
+    console.log("Post Executed!");
+    try {
+        connection = await oracledb.getConnection({
+            user: "system",
+            password: "ak",
+            connectString: "XE"
+        });
+
+        const result = await connection.execute(
+            `SELECT * FROM users WHERE name = :name AND password = :password`,
+            {
+                name: req.body.username,
+                password: req.body.password
+            }
+        );
+
+        if (result.rows.length > 0) {
+            res.send({ success: true });
+        } else {
+            res.send({ success: false });
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    } finally {
+        if (connection) {
+            connection.close();
         }
     }
 });
